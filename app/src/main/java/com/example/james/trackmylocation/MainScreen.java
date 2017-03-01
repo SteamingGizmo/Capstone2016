77f9589e7b3815a4af87d66921c721af522bc6a2
James <jamie8@shaw.ca>
2017-03-01 15:49:01 -0800

    Created Background Service

--- app/src/main/java/com/example/james/trackmylocation/MainScreen.java
+++ app/src/main/java/com/example/james/trackmylocation/MainScreen.java
@@ -1,23 +1,20 @@
 package com.example.james.trackmylocation;
 
-import android.Manifest;
+
 import android.app.Activity;
-import android.content.pm.PackageManager;
+import android.content.Intent;
+
 import android.location.Location;
 import android.os.AsyncTask;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
-import android.widget.CompoundButton;
-import android.widget.ListView;
+
 import android.widget.Spinner;
-import android.widget.Switch;
-import android.widget.TextView;
+
+
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -26,14 +23,12 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.android.gms.common.ConnectionResult;
+
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
-import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
-import com.google.android.gms.gcm.GoogleCloudMessaging;
+
 import com.google.android.gms.iid.InstanceID;
 import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
+
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -45,10 +40,6 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PointOfInterest;
 
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -56,14 +47,9 @@ import java.util.Map;
 
 
 public class MainScreen extends AppCompatActivity implements
-        ConnectionCallbacks,
-        OnConnectionFailedListener,
-        LocationListener,
         OnMapReadyCallback,
         GoogleMap.OnPoiClickListener{
 
-    private Switch locationswitch;
-    private GPSTracker gps;
     private Spinner spinner;
     double longitude, latitude;
     String PROJECT_ID = "location-tracker-1478206108627";
@@ -75,45 +61,32 @@ public class MainScreen extends AppCompatActivity implements
     String uniqueid;
     Activity activity;
 
-    //Google Api Client For Location Support
-    GoogleApiClient googleApiClient;
-
-    //Location Objects
-    Location locationobj;
-    LocationRequest locationRequest;
-    LocationListener locationListener;
-
 
     //Map Objects
     MapFragment mapfragment;
     GoogleMap gMap;
 
 
-
-    String url = "http://35.164.7.20/FirstTry.php";
     String bus_url = "http://35.164.7.20/BusSchedule.php";
     String reduced_id,temp_reduced_id;
 
+
+    Intent background_service;
+    Boolean service_started = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
         activity = this;
-        //token = InstanceID.getInstance(this).getToken(authorizedEntity, scope);
+
         //Generate Map Fragment
         mapfragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapfragment.getMapAsync(this);
+        background_service = new Intent(getBaseContext(),transmitlocation_background_service.class);
+
 
         addItemsOnSpinner();
-
-
-        googleApiClient = new GoogleApiClient.Builder(this)
-                .addApi(LocationServices.API)
-                .addConnectionCallbacks(this)
-                .build();
-
-
 
         Asyncgetiid asyncgetiid = new Asyncgetiid();
         asyncgetiid.execute();
@@ -131,75 +104,19 @@ public class MainScreen extends AppCompatActivity implements
     protected void onResume() {
         super.onResume();
         Log.i("Test", "onResume: is running ");
-        googleApiClient.connect();
+//        googleApiClient.connect();
 
 
     }
+
 
     @Override
-    protected void onStop() {
-        super.onStop();
-        googleApiClient.disconnect();
-
+    protected void onDestroy(){
+        super.onDestroy();
+        activity.stopService(background_service);
     }
 
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-         int test_constant = 1;
-        //ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, test_constant);
-        locationobj = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
-        if (locationobj != null){
 
-            UpdateMap();
-
-        }
-        locationListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-                locationobj = location;
-                UpdateMap();
-                }
-        };
-        setLocationRequest();
-        startlocationupdates();
-
-        }
-
-    public void setLocationRequest(){
-        locationRequest = new LocationRequest();
-        locationRequest.setInterval(10000);
-        locationRequest.setFastestInterval(5000);
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-    }
-
-    public void startlocationupdates(){
-        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient,locationRequest,locationListener);
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-
-    }
-    public void UpdateMap(){
-        float zoomLevel = 19.0f;
-
-        gMap.addMarker(new MarkerOptions()
-                .position(new LatLng(locationobj.getLatitude(),locationobj.getLongitude()))
-                .title("Last Position"));
-        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(locationobj.getLatitude(),locationobj.getLongitude()),zoomLevel));
-        sendlocation();
-
-    }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -208,39 +125,7 @@ public class MainScreen extends AppCompatActivity implements
         gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(49.882114,-119.477829),10.0f));
     }
 
-    public void sendlocation(){
-        RequestQueue Queue = Volley.newRequestQueue(this);
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                Log.d("ServerResponce",response);
-            }
 
-        },new Response.ErrorListener(){
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-                // Error handling
-                // System.out.println("Something went wrong!");
-                error.printStackTrace();
-                Log.d("Connection Error", error.getMessage());
-            }
-        })
-        {
-            @Override
-            protected Map<String, String> getParams()
-            {
-                Map<String, String> params = new HashMap<>();
-                // the POST parameters:
-                params.put("lati", String.valueOf(locationobj.getLatitude()));
-                params.put("longi", String.valueOf(locationobj.getLongitude()));
-                params.put("UniqueID",reduced_id.substring(0,64));
-                Log.d("UNIID",reduced_id.substring(0,64));
-                return params;
-            }
-        };
-        Queue.add(stringRequest);
-    }
     private class Asyncgetiid extends AsyncTask<String,String,String>{
 
         String grabid;
@@ -261,7 +146,11 @@ public class MainScreen extends AppCompatActivity implements
             uniqueid = iid;
             temp_reduced_id = uniqueid.replaceAll("[^\\w\\s]","");
             reduced_id = temp_reduced_id.replaceAll("_","");
-
+            if(!service_started){
+                background_service.putExtra("iid",reduced_id);
+                activity.startService(background_service);
+                service_started = true;
+            }
         }
 
     }
@@ -294,7 +183,7 @@ public class MainScreen extends AppCompatActivity implements
            @Override
            public void onResponse(String response) {
 
-               //next_bus = response;
+               next_bus = response;
 
                Log.i("EchoResponce", next_bus);
 
--- app/src/main/java/com/example/james/trackmylocation/transmitlocation_background_service.java
+++ app/src/main/java/com/example/james/trackmylocation/transmitlocation_background_service.java
@@ -1,22 +1,133 @@
 package com.example.james.trackmylocation;
 
 import android.app.IntentService;
+import android.app.Service;
 import android.content.Intent;
+import android.location.Location;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.appdatasearch.GetRecentContextCall;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Queue;
 
 
 /**
  * Created by James on 2/16/2017.
  */
 
-public class transmitlocation_background_service extends IntentService {
+public class transmitlocation_background_service extends Service implements  GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
 
-    public transmitlocation_background_service(){
-        super("Location_Transmistion_Service");
+    GoogleApiClient googleApiClient;
+    Location location;
+    LocationRequest locationRequest;
+    LocationListener locationListener;
+    String url = "http://35.164.7.20/FirstTry.php";
+    String iid;
+
+
+    @Override
+    public void onCreate(){
+        Log.i("Background Service","Ran");
+        Toast.makeText(this,"Service Running",Toast.LENGTH_SHORT).show();
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .build();
+
+
     }
     @Override
-    protected void onHandleIntent(Intent workIntent){
+    public void onConnected(@Nullable Bundle bundle){
+    location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+            sendlocation();
+            }
+        };
+        setLocationRequest();
+    LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient,locationRequest,locationListener);
+    }
+    public void setLocationRequest(){
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i){
 
     }
 
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult){
 
+    }
+    public void sendlocation(){
+        RequestQueue Queue = Volley.newRequestQueue(this);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("ServerResponce",response);
+            }
+
+        },new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                // Error handling
+                // System.out.println("Something went wrong!");
+                error.printStackTrace();
+                Log.d("Connection Error", error.getMessage());
+            }
+        })
+        {
+            @Override
+            protected Map<String, String> getParams()
+            {
+                Map<String, String> params = new HashMap<>();
+                // the POST parameters:
+                params.put("lati", String.valueOf(location.getLatitude()));
+                params.put("longi", String.valueOf(location.getLongitude()));
+                params.put("UniqueID",iid.substring(0,64));
+                //Log.d("UNIID",reduced_id.substring(0,64));
+                return params;
+            }
+        };
+        Queue.add(stringRequest);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId){
+        iid = intent.getStringExtra("iid");
+        googleApiClient.connect();
+        return START_STICKY;
+    }
+    @Override
+    public void onDestroy(){
+
+    }
+    @Override
+    public IBinder onBind(Intent intent){
+        return null;
+    }
 }
